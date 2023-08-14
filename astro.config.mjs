36be3a7053be1d265b684ae437c214b0cfc48c60
scott-salter <scottsalter2@gmail.com>
2023-08-14 15:53:06 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { SITE_URL } from "./src/data/config";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), robotsTxt()],
   site: 'https://scott-salter.com',
   markdown: {
     syntaxHighlight: "shiki",
